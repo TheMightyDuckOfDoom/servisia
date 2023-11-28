0b4f92ef4dc0a6ceb1900a98fb6a2856b5d566ee
Tobias Senti <git@tsenti.li>
2023-11-28 15:53:54 +0000

    Added inout sram interface

--- rtl/servisia.v
+++ rtl/servisia.v
@@ -11,17 +11,35 @@ module servisia (
     output wire [num_gpios-1:0] q_o,
 
     // SRAM interface
-    output wire [aw-1:0] sram_waddr_o,
-    output wire [7:0]    sram_wdata_o,
-    output wire          sram_wen_o,
-
-    output wire [aw-1:0] sram_raddr_o,
-    input wire [7:0]     sram_rdata_i,
-    output wire          sram_ren_o
+    output wire          sram_cs_no,
+    output wire          sram_wen_no,
+    output wire [aw-1:0] sram_addr_o,
+    inout  wire [7:0]    sram_data_io
 );
     parameter memsize = 65536;
     parameter aw = $clog2(memsize);
     parameter num_gpios = 1;
+
+    wire sram_wen, sram_ren;
+    wire [aw-1:0] sram_raddr, sram_waddr;
+    wire [7:0] sram_wdata, sram_rdata;
+
+    assign sram_cs_no = !(sram_wen | sram_ren);
+    assign sram_addr_o = !sram_wen_no ? sram_waddr : sram_raddr;
+
+    assign sram_wen_no = !sram_wen;
+    assign sram_rdata = sram_ren ? sram_data_io : 8'd0;
+
+    generate
+        genvar i;
+        for(i = 0; i < 8; i = i + 1) begin
+            ZBUF_74LVC1G125 i_data_zbuf (
+                .A    ( sram_wdata[i]   ),
+                .EN_N ( sram_wen_no     ),
+                .Y    ( sram_data_io[i] )
+            );
+        end
+    endgenerate
 
     subservient #(
         .memsize  ( memsize ),
@@ -32,12 +50,12 @@ module servisia (
         .i_rst ( !rst_ni ),
 
         //SRAM interface
-        .o_sram_waddr ( sram_waddr_o ),
-        .o_sram_wdata ( sram_wdata_o ),
-        .o_sram_wen   ( sram_wen_o   ),
-        .o_sram_raddr ( sram_raddr_o ),
-        .i_sram_rdata ( sram_rdata_i ),
-        .o_sram_ren   ( sram_ren_o   ),
+        .o_sram_waddr ( sram_waddr ),
+        .o_sram_wdata ( sram_wdata ),
+        .o_sram_wen   ( sram_wen   ),
+        .o_sram_raddr ( sram_raddr ),
+        .i_sram_rdata ( sram_rdata ),
+        .o_sram_ren   ( sram_ren   ),
 
         //Debug interface
         .i_debug_mode (  1'b0 ),
